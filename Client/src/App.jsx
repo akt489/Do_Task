93843075247ace83a85ddacad353a6984a89725e
Abraham <abthilahun@gamil.com>
2026-04-21 18:46:04 +0300

    first file structure

---
+++ Client/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div>
+      <p>react connected successfully</p>
+    </div>
+  )
+}
+
+export default App;
---
+++ Endback/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../Client")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Client/public/index.html"));
+});
+
+app.listen(5011, () => {
+  console.log("Server is running on port 5011");
+});
